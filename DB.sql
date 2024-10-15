668da80bed42f54daf5b113d130fd5b7bc75b8fb
Pive0528 <pive0528@gmail.com>
2024-10-15 15:28:05 +0900

    helloWorldJob 생성 후 실행

---
+++ DB.sql
@@ -0,0 +1,9 @@
+DROP DATABASE IF EXISTS SpringBatch_10;
+CREATE DATABASE SpringBatch_10;
+USE SpringBatch_10;
+
+DROP DATABASE IF EXISTS SpringBatch_10_backup;
+CREATE DATABASE SpringBatch_10_backup;
+USE SpringBatch_10;
+
+SHOW TABLES;
